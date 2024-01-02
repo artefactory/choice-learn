16d1856c1afd24741c9386fe3c6f201a129093ec
VincentAuriau <auriau.vincent@gmail.com>
2024-01-02 17:14:53 +0100

    ENH: import level for data & models

--- choice_learn/data/__init__.py
+++ choice_learn/data/__init__.py
@@ -1,1 +1,5 @@
 """Data handling classes and functions."""
+from .choice_dataset import ChoiceDataset
+from .store import FeaturesStore, OneHotStore
+
+__all__ = ["ChoiceDataset", "FeaturesStore", "OneHotStore"]
--- choice_learn/models/__init__.py
+++ choice_learn/models/__init__.py
@@ -1,1 +1,6 @@
 """Models classes and functions."""
+
+from .conditional_mnl import ConditionalMNL, ModelSpecification
+from .rumnet import PaperRUMnet as RUMnet
+
+__all__ = ["ModelSpecification", "ConditionalMNL", "RUMnet"]
