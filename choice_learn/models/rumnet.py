FIX: softmax/mean dimensions after U

choice_learn/models/rumnet.py
@@ -910,8 +910,8 @@ class GPURUMnet(PaperRUMnet):
                 contexts_items_availabilities=contexts_items_availabilities,
                 choices=choices,
             )
-
-            eps_probabilities = tf.nn.softmax(all_u, axis=1)
+            print(all_u.shape)
+            eps_probabilities = tf.nn.softmax(all_u, axis=2)
             # Average probabilities over heterogeneities
             probabilities = tf.reduce_mean(eps_probabilities, axis=1)
 
@@ -982,15 +982,15 @@ class GPURUMnet(PaperRUMnet):
         tf.Tensor (batch_size, n_items)
             Probabilities for each product to be chosen for each session
         """
-        utilities = self.compute_utility(
+        utilities = self.compute_batch_utility(
             fixed_items_features=fixed_items_features,
             contexts_features=contexts_features,
             contexts_items_features=contexts_items_features,
             contexts_items_availabilities=contexts_items_availabilities,
             choices=choices,
         )
-        probabilities = tf.nn.softmax(utilities, axis=1)
-        probabilities = tf.reduce_mean(probabilities, axis=-1)
+        probabilities = tf.nn.softmax(utilities, axis=2)
+        probabilities = tf.reduce_mean(probabilities, axis=1)
 
         # Test with availability normalization
         probabilities = tf.multiply(probabilities, contexts_items_availabilities)