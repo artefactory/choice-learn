0982574ead713e528be3dfe63528b4a2d857efa0
VincentAuriau <auriau.vincent@gmail.com>
2024-03-08 19:09:38 +0100

    ADD: TaFeng downloadable from choice_learn.datasets

--- choice_learn/datasets/__init__.py
+++ choice_learn/datasets/__init__.py
@@ -1,5 +1,13 @@
 """Init file for datasets module."""
 
 from .base import load_electricity, load_heating, load_modecanada, load_swissmetro, load_train
+from .examples import load_tafeng
 
-__all__ = ["load_modecanada", "load_swissmetro", "load_electricity", "load_heating", "load_train"]
+__all__ = [
+    "load_modecanada",
+    "load_swissmetro",
+    "load_electricity",
+    "load_heating",
+    "load_train",
+    "load_tafeng",
+]
