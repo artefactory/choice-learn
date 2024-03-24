de0762cf9aa3f23eb1cbd3eee37ef7af97496e23
VincentAURIAU <auriau.vincent@gmail.com>
2024-03-24 21:40:34 +0100

    ADD: tf_ops base tests

--- tests/unit_tests/tf_ops.py
+++ tests/unit_tests/tf_ops.py
@@ -11,8 +11,37 @@ def test_softmax():
     availabilities = np.array([[1., 1., 1., 1.],
                                [1., 0., 1., 0.],
                                [1., 1., 1., 1.]])
+    """
     probabilities = np.array([[0.25, 0.25, 0.25, 0.25],
                               [0.5, 0.0, 0.5, 0.0],
                               [0.125, 0.125, 0.250, 0.5]])
-    
-    assert (softmax_with_availabilities(items_logit_by_choice=logits, available_items_by_choice=availabilities) == probabilities).all()
+    """
+    softmax_probabilities = softmax_with_availabilities(items_logit_by_choice=logits, available_items_by_choice=availabilities).numpy()
+    assert (softmax_probabilities[0] < 0.26).all()
+    assert (softmax_probabilities[0] > 0.24).all()
+
+    assert (softmax_probabilities[1][[0, 2]] < 0.51).all()
+    assert (softmax_probabilities[1][[0, 2]] > 0.49).all()
+    assert (softmax_probabilities[1][[1, 3]] < 0.01).all()
+
+    assert (softmax_probabilities[2][0] < 0.126).all()
+    assert (softmax_probabilities[2][0] > 0.124).all()
+    assert (softmax_probabilities[2][1] < 0.126).all()
+    assert (softmax_probabilities[2][1] > 0.124).all()
+    assert (softmax_probabilities[2][2] < 0.251).all()
+    assert (softmax_probabilities[2][2] > 0.249).all()
+    assert (softmax_probabilities[2][3] < 0.501).all()
+    assert (softmax_probabilities[2][3] > 0.499).all()
+
+def test_CustomCategoricalCrossEntropy():
+    loss = CustomCategoricalCrossEntropy()
+    assert loss([[0., 1.]], [[0., 1.]]) == 0.0
+    assert loss([[0., 1.]], [[1., 0.]]) > 20.0
+
+    loss = CustomCategoricalCrossEntropy(sparse=True)
+    assert loss([1], [[0., 1.]]) == 0.0
+    assert loss([1], [[1., 0.]]) > 20.0
+
+
+    loss = CustomCategoricalCrossEntropy(sparse=True, from_logits=True)
+    assert loss([1], [[21., 0.]]) > 20.0
