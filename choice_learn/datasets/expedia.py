62eacc1de2edf7c819e648ec270c3cb11eb3c969
VincentAURIAU <auriau.vincent@gmail.com>
2024-04-04 18:23:05 +0200

    ADD: expedia rumnet preprocessing now works with cache

--- choice_learn/datasets/expedia.py
+++ choice_learn/datasets/expedia.py
@@ -10,6 +10,7 @@ from choice_learn.data.storage import OneHotStorage
 from choice_learn.datasets.base import get_path
 
 DATA_MODULE = "choice_learn.datasets.data"
+CACHE_MODULE = "choice_learn.datasets.cache"
 
 
 def load_expedia(as_frame=False, preprocessing="rumnet"):
@@ -39,120 +40,123 @@ def load_expedia(as_frame=False, preprocessing="rumnet"):
 
     if preprocessing == "rumnet":
         logging.info("rumnet preprocessing selected, starting preprocessing...")
-        expedia_df.date_time = pd.to_datetime(expedia_df.date_time, format="%Y-%m-%d %H:%M:%S")
-        expedia_df.loc[:, "day_of_week"] = expedia_df.loc[:, "date_time"].dt.dayofweek
-        expedia_df.loc[:, "month"] = expedia_df.loc[:, "date_time"].dt.month
-        expedia_df.loc[:, "hour"] = expedia_df.loc[:, "date_time"].dt.hour
+        try:
+            expedia_df = pd.read_csv(get_path("preprocessed_expedia_rumnet.csv", module=DATA_MODULE), engine="pyarrow")
+        except FileNotFoundError:
+            expedia_df.date_time = pd.to_datetime(expedia_df.date_time, format="%Y-%m-%d %H:%M:%S")
+            expedia_df.loc[:, "day_of_week"] = expedia_df.loc[:, "date_time"].dt.dayofweek
+            expedia_df.loc[:, "month"] = expedia_df.loc[:, "date_time"].dt.month
+            expedia_df.loc[:, "hour"] = expedia_df.loc[:, "date_time"].dt.hour
 
-        logging.info("Filtering ids with less than 1000 occurrences")
-        for id_col in [
-            "site_id",
-            "visitor_location_country_id",
-            "prop_country_id",
-            "srch_destination_id",
-        ]:
-            value_counts = expedia_df[["srch_id", id_col]].drop_duplicates()[id_col].value_counts()
-            kept_ids = value_counts.index[value_counts.gt(1000)]
-            for id_ in expedia_df[id_col].unique():
-                if id_ not in kept_ids:
-                    expedia_df.loc[expedia_df[id_col] == id_, id_col] = -1
+            logging.info("Filtering ids with less than 1000 occurrences")
+            for id_col in [
+                "site_id",
+                "visitor_location_country_id",
+                "prop_country_id",
+                "srch_destination_id",
+            ]:
+                value_counts = expedia_df[["srch_id", id_col]].drop_duplicates()[id_col].value_counts()
+                kept_ids = value_counts.index[value_counts.gt(1000)]
+                for id_ in expedia_df[id_col].unique():
+                    if id_ not in kept_ids:
+                        expedia_df.loc[expedia_df[id_col] == id_, id_col] = -1
 
-        logging.info("Filtering DF for price, stay length, booking window, etc.")
-        # Filtering
-        expedia_df = expedia_df[expedia_df.price_usd <= 1000]
-        expedia_df = expedia_df[expedia_df.price_usd >= 10]
-        expedia_df["log_price"] = expedia_df.price_usd.apply(np.log)
-        expedia_df = expedia_df[expedia_df.srch_length_of_stay <= 14]
-        expedia_df = expedia_df[expedia_df.srch_booking_window <= 365]
-        expedia_df["booking_window"] = np.log(expedia_df["srch_booking_window"] + 1)
-        expedia_df = expedia_df.fillna(-1)
+            logging.info("Filtering DF for price, stay length, booking window, etc.")
+            # Filtering
+            expedia_df = expedia_df[expedia_df.price_usd <= 1000]
+            expedia_df = expedia_df[expedia_df.price_usd >= 10]
+            expedia_df["log_price"] = expedia_df.price_usd.apply(np.log)
+            expedia_df = expedia_df[expedia_df.srch_length_of_stay <= 14]
+            expedia_df = expedia_df[expedia_df.srch_booking_window <= 365]
+            expedia_df["booking_window"] = np.log(expedia_df["srch_booking_window"] + 1)
+            expedia_df = expedia_df.fillna(-1)
 
-        logging.info("Sorting DF columns")
-        order_cols = [
-            "srch_id",
-            "prop_id",
-            "prop_starrating",
-            "prop_review_score",
-            "prop_brand_bool",
-            "prop_location_score1",
-            "prop_location_score2",
-            "prop_log_historical_price",
-            "position",
-            "promotion_flag",
-            "srch_length_of_stay",
-            "srch_adults_count",
-            "srch_children_count",
-            "srch_room_count",
-            "srch_saturday_night_bool",
-            "orig_destination_distance",
-            "random_bool",
-            "day_of_week",
-            "month",
-            "hour",
-            "log_price",
-            "booking_window",
-            "site_id",
-            "visitor_location_country_id",
-            "prop_country_id",
-            "srch_destination_id",
-            "click_bool",
-            "booking_bool",
-        ]
-        expedia_df = expedia_df[order_cols]
+            logging.info("Sorting DF columns")
+            order_cols = [
+                "srch_id",
+                "prop_id",
+                "prop_starrating",
+                "prop_review_score",
+                "prop_brand_bool",
+                "prop_location_score1",
+                "prop_location_score2",
+                "prop_log_historical_price",
+                "position",
+                "promotion_flag",
+                "srch_length_of_stay",
+                "srch_adults_count",
+                "srch_children_count",
+                "srch_room_count",
+                "srch_saturday_night_bool",
+                "orig_destination_distance",
+                "random_bool",
+                "day_of_week",
+                "month",
+                "hour",
+                "log_price",
+                "booking_window",
+                "site_id",
+                "visitor_location_country_id",
+                "prop_country_id",
+                "srch_destination_id",
+                "click_bool",
+                "booking_bool",
+            ]
+            expedia_df = expedia_df[order_cols]
 
-        logging.info("Creating dummy availabilities")
-        expedia_df["av"] = 1
-        asst_size = 38  # Fixed number of items in the assortment
+            logging.info("Creating dummy availabilities")
+            expedia_df["av"] = 1
+            asst_size = 38  # Fixed number of items in the assortment
 
-        logging.info("Creating dummy products to reach assortment size")
-        # Loop to fill the data frame with dummy products
-        # next loop creates the dummy products
-        for _ in range(asst_size):
-            dum = (
+            logging.info("Creating dummy products to reach assortment size")
+            # Loop to fill the data frame with dummy products
+            # next loop creates the dummy products
+            for _ in range(asst_size):
+                dum = (
+                    expedia_df.groupby("srch_id")
+                    .filter(lambda x: len(x) < asst_size)
+                    .groupby("srch_id")
+                    .max()
+                    .reset_index(drop=False)
+                )
+                dum.loc[:, "booking_bool"] = 0
+                dum.loc[:, "av"] = 0
+                expedia_df = pd.concat([expedia_df, dum])
+
+            # getting rid of search & prop_id and the clickbool and bookingbool
+            # adding no_purchase fixed effect
+            expedia_df["is_no_purchase"] = 0
+
+            logging.info("Creating the no purchase option")
+            # adding the no_purchase option to the data
+            df1 = (
                 expedia_df.groupby("srch_id")
-                .filter(lambda x: len(x) < asst_size)
+                .filter(lambda x: x.booking_bool.sum() == 1)
                 .groupby("srch_id")
                 .max()
                 .reset_index(drop=False)
             )
-            dum.loc[:, "booking_bool"] = 0
-            dum.loc[:, "av"] = 0
-            expedia_df = pd.concat([expedia_df, dum])
+            df1.loc[:, "is_no_purchase"] = 1
+            df1.loc[:, "log_price"] = 0
+            df1.loc[:, "booking_bool"] = 0
 
-        # getting rid of search & prop_id and the clickbool and bookingbool
-        # adding no_purchase fixed effect
-        expedia_df["is_no_purchase"] = 0
+            df2 = (
+                expedia_df.groupby("srch_id")
+                .filter(lambda x: x.booking_bool.sum() == 0)
+                .groupby("srch_id")
+                .max()
+                .reset_index(drop=False)
+            )
+            df2.loc[:, "is_no_purchase"] = 1
+            df2.loc[:, "log_price"] = 0
+            df2.loc[:, "booking_bool"] = 1
+            expedia_df = pd.concat([expedia_df, df1, df2])
 
-        logging.info("Creating the no purchase option")
-        # adding the no_purchase option to the data
-        df1 = (
-            expedia_df.groupby("srch_id")
-            .filter(lambda x: x.booking_bool.sum() == 1)
-            .groupby("srch_id")
-            .max()
-            .reset_index(drop=False)
-        )
-        df1.loc[:, "is_no_purchase"] = 1
-        df1.loc[:, "log_price"] = 0
-        df1.loc[:, "booking_bool"] = 0
-
-        df2 = (
-            expedia_df.groupby("srch_id")
-            .filter(lambda x: x.booking_bool.sum() == 0)
-            .groupby("srch_id")
-            .max()
-            .reset_index(drop=False)
-        )
-        df2.loc[:, "is_no_purchase"] = 1
-        df2.loc[:, "log_price"] = 0
-        df2.loc[:, "booking_bool"] = 1
-        expedia_df = pd.concat([expedia_df, df1, df2])
-
-        logging.info("Sorting the data frame")
-        expedia_df = expedia_df.sort_values("srch_id")
-        expedia_df.to_csv(
-            get_path("expedia_rumnet_preprocessing.csv", module=DATA_MODULE), index=False
-        )
+            logging.info("Sorting the data frame")
+            expedia_df = expedia_df.sort_values("srch_id")
+            expedia_df.to_csv(
+                get_path("preprocessed_expedia_rumnet.csv", module=DATA_MODULE), index=False
+            )
 
         choices = expedia_df.groupby("srch_id").apply(lambda x: x.booking_bool.argmax())
 
