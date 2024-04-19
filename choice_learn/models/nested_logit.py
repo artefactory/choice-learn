cb45210e6c7d76ed216fd284b84695069caf7c83
VincentAuriau <auriau.vincent@gmail.com>
2024-04-19 18:46:13 +0200

    ADD: finalized 1st version of Nested Logit

--- choice_learn/models/nested_logit.py
+++ choice_learn/models/nested_logit.py
@@ -1,5 +1,8 @@
 """Implementation of the Nested Logit model."""
+import logging
+
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 from choice_learn.models.base_model import ChoiceModel
@@ -281,3 +284,356 @@ class NestedLogit(ChoiceModel):
                 raise ValueError(f"Mode {mode} not recognized.")
 
         self.coefficients = coefficients
+
+    def _store_dataset_features_names(self, dataset):
+        """Register the name of the features in the dataset. For later use in utility computation.
+
+        Parameters
+        ----------
+        dataset : ChoiceDataset
+            ChoiceDataset used to fit the model.
+        """
+        self._shared_features_by_choice_names = dataset.shared_features_by_choice_names
+        self._items_features_by_choice_names = dataset.items_features_by_choice_names
+
+    def compute_batch_utility(
+        self,
+        shared_features_by_choice,
+        items_features_by_choice,
+        available_items_by_choice,
+        choices,
+        verbose=1,
+    ):
+        """Compute the utility when the model is constructed from a MNLCoefficients object.
+
+        Parameters
+        ----------
+        shared_features_by_choice : tuple of np.ndarray (choices_features)
+            a batch of shared features
+            Shape must be (n_choices, n_shared_features)
+        items_features_by_choice : tuple of np.ndarray (choices_items_features)
+            a batch of items features
+            Shape must be (n_choices, n_items_features)
+        available_items_by_choice : np.ndarray
+            A batch of items availabilities
+            Shape must be (n_choices, n_items)
+        choices: np.ndarray
+            Choices
+            Shape must be (n_choices, )
+        verbose : int, optional
+            Parametrization of the logging outputs, by default 1
+
+        Returns
+        -------
+        tf.Tensor
+            Utilities corresponding of shape (n_choices, n_items)
+        """
+        _ = choices
+
+        n_items = available_items_by_choice.shape[1]
+        n_choices = available_items_by_choice.shape[0]
+        items_utilities_by_choice = []
+
+        if not isinstance(shared_features_by_choice, tuple):
+            shared_features_by_choice = (shared_features_by_choice,)
+        if not isinstance(items_features_by_choice, tuple):
+            items_features_by_choice = (items_features_by_choice,)
+
+        # Shared features
+        if self._shared_features_by_choice_names is not None:
+            for i, feat_tuple in enumerate(self._shared_features_by_choice_names):
+                for j, feat in enumerate(feat_tuple):
+                    if feat in self.coefficients.features_with_weights:
+                        (
+                            item_index_list,
+                            weight_index_list,
+                        ) = self.coefficients.get_weight_item_indexes(feat)
+                        for item_index, weight_index in zip(item_index_list, weight_index_list):
+                            partial_items_utility_by_choice = tf.zeros((n_choices, n_items))
+                            partial_items_utility_by_choice = [
+                                tf.zeros(n_choices) for _ in range(n_items)
+                            ]
+
+                            for q, idx in enumerate(item_index):
+                                if isinstance(idx, list):
+                                    for k in idx:
+                                        tf.cast(shared_features_by_choice[i][:, j], tf.float32)
+                                        compute = tf.multiply(
+                                            shared_features_by_choice[i][:, j],
+                                            self.trainable_weights[weight_index][:, q],
+                                        )
+                                        partial_items_utility_by_choice[k] += compute
+                                else:
+                                    compute = tf.multiply(
+                                        tf.cast(shared_features_by_choice[i][:, j], tf.float32),
+                                        self.trainable_weights[weight_index][:, q],
+                                    )
+                                    partial_items_utility_by_choice[idx] += compute
+
+                            items_utilities_by_choice.append(
+                                tf.cast(
+                                    tf.stack(partial_items_utility_by_choice, axis=1), tf.float32
+                                )
+                            )
+                    elif verbose > 0:
+                        logging.info(
+                            f"Feature {feat} is in dataset but has no weight assigned\
+                                in utility computations"
+                        )
+
+        # Items features
+        if self._items_features_by_choice_names is not None:
+            for i, feat_tuple in enumerate(self._items_features_by_choice_names):
+                for j, feat in enumerate(feat_tuple):
+                    if feat in self.coefficients.features_with_weights:
+                        (
+                            item_index_list,
+                            weight_index_list,
+                        ) = self.coefficients.get_weight_item_indexes(feat)
+                        for item_index, weight_index in zip(item_index_list, weight_index_list):
+                            partial_items_utility_by_choice = tf.zeros((n_choices, n_items))
+
+                            for q, idx in enumerate(item_index):
+                                if isinstance(idx, list):
+                                    for k in idx:
+                                        partial_items_utility_by_choice = tf.concat(
+                                            [
+                                                partial_items_utility_by_choice[:, :k],
+                                                tf.expand_dims(
+                                                    tf.multiply(
+                                                        tf.cast(
+                                                            items_features_by_choice[i][:, k, j],
+                                                            tf.float32,
+                                                        ),
+                                                        self.trainable_weights[weight_index][:, q],
+                                                    ),
+                                                    axis=-1,
+                                                ),
+                                                partial_items_utility_by_choice[:, k + 1 :],
+                                            ],
+                                            axis=1,
+                                        )
+                                else:
+                                    partial_items_utility_by_choice = tf.concat(
+                                        [
+                                            partial_items_utility_by_choice[:, :idx],
+                                            tf.expand_dims(
+                                                tf.multiply(
+                                                    tf.cast(
+                                                        items_features_by_choice[i][:, idx, j],
+                                                        tf.float32,
+                                                    ),
+                                                    self.trainable_weights[weight_index][:, q],
+                                                ),
+                                                axis=-1,
+                                            ),
+                                            partial_items_utility_by_choice[:, idx + 1 :],
+                                        ],
+                                        axis=1,
+                                    )
+
+                            items_utilities_by_choice.append(
+                                tf.cast(partial_items_utility_by_choice, tf.float32)
+                            )
+                    elif verbose > 0:
+                        logging.info(
+                            f"Feature {feat} is in dataset but has no weight assigned\
+                                in utility computations"
+                        )
+
+        if "intercept" in self.coefficients.features_with_weights:
+            item_index_list, weight_index_list = self.coefficients.get_weight_item_indexes(
+                "intercept"
+            )
+
+            for item_index, weight_index in zip(item_index_list, weight_index_list):
+                partial_items_utility_by_choice = tf.zeros((n_items,))
+                for q, idx in enumerate(item_index):
+                    partial_items_utility_by_choice = tf.concat(
+                        [
+                            partial_items_utility_by_choice[:idx],
+                            self.trainable_weights[weight_index][:, q],
+                            partial_items_utility_by_choice[idx + 1 :],
+                        ],
+                        axis=0,
+                    )
+
+                partial_items_utility_by_choice = tf.stack(
+                    [partial_items_utility_by_choice] * n_choices, axis=0
+                )
+
+                items_utilities_by_choice.append(
+                    tf.cast(partial_items_utility_by_choice, tf.float32)
+                )
+
+        return tf.reduce_sum(items_utilities_by_choice, axis=0)
+
+    def fit(self, choice_dataset, get_report=False, **kwargs):
+        """Fit function to estimate the paramters.
+
+        Parameters
+        ----------
+        choice_dataset : ChoiceDataset
+            Choice dataset to use for the estimation.
+        get_report: bool, optional
+            Whether or not to compute a report of the estimation, by default False
+
+        Returns
+        -------
+        dict
+            dict with fit history.
+        """
+        self.instantiate(choice_dataset)
+
+        fit = super().fit(choice_dataset=choice_dataset, **kwargs)
+        if get_report:
+            self.report = self.compute_report(choice_dataset)
+        return fit
+
+    def _fit_with_lbfgs(
+        self,
+        choice_dataset,
+        sample_weight=None,
+        get_report=False,
+        **kwargs,
+    ):
+        """Specific fit function to estimate the paramters with LBFGS.
+
+        Parameters
+        ----------
+        choice_dataset : ChoiceDataset
+            Choice dataset to use for the estimation.
+        n_epochs : int
+            Number of epochs to run.
+        tolerance : float, optional
+            Tolerance in the research of minimum, by default 1e-8
+        get_report: bool, optional
+            Whether or not to compute a report of the estimation, by default False
+
+        Returns
+        -------
+        dict
+            dict with fit history.
+        """
+        self.instantiate(choice_dataset)
+
+        fit = super()._fit_with_lbfgs(
+            dataset=choice_dataset,
+            sample_weight=sample_weight,
+            **kwargs,
+        )
+        if get_report:
+            self.report = self.compute_report(choice_dataset)
+        return fit
+
+    def compute_report(self, dataset):
+        """Compute a report of the estimated weights.
+
+        Parameters
+        ----------
+        dataset : ChoiceDataset
+            ChoiceDataset used for the estimation of the weights that will be
+            used to compute the Std Err of this estimation.
+
+        Returns
+        -------
+        pandas.DataFrame
+            A DF with estimation, Std Err, z_value and p_value for each coefficient.
+        """
+        import tensorflow_probability as tfp
+
+        weights_std = self.get_weights_std(dataset)
+        dist = tfp.distributions.Normal(loc=0.0, scale=1.0)
+
+        names = []
+        z_values = []
+        estimations = []
+        p_z = []
+        i = 0
+        for weight in self.trainable_weights:
+            for j in range(weight.shape[1]):
+                names.append(f"{weight.name}_{j}")
+                estimations.append(weight.numpy()[0][j])
+                z_values.append(weight.numpy()[0][j] / weights_std[i].numpy())
+                p_z.append(2 * (1 - dist.cdf(tf.math.abs(z_values[-1])).numpy()))
+                i += 1
+
+        return pd.DataFrame(
+            {
+                "Coefficient Name": names,
+                "Coefficient Estimation": estimations,
+                "Std. Err": weights_std.numpy(),
+                "z_value": z_values,
+                "P(.>z)": p_z,
+            },
+        )
+
+    def get_weights_std(self, dataset):
+        """Approximates Std Err with Hessian matrix.
+
+        Parameters
+        ----------
+        dataset : ChoiceDataset
+            ChoiceDataset used for the estimation of the weights that will be
+            used to compute the Std Err of this estimation.
+
+        Returns
+        -------
+        tf.Tensor
+            Estimation of the Std Err for the weights.
+        """
+        # Loops of differentiation
+        with tf.GradientTape() as tape_1:
+            with tf.GradientTape(persistent=True) as tape_2:
+                model = self.clone()
+                w = tf.concat(self.trainable_weights, axis=1)
+                tape_2.watch(w)
+                tape_1.watch(w)
+                mw = []
+                index = 0
+                for _w in self.trainable_weights:
+                    mw.append(w[:, index : index + _w.shape[1]])
+                    index += _w.shape[1]
+                model.trainable_weights = mw
+                for batch in dataset.iter_batch(batch_size=-1):
+                    utilities = model.compute_batch_utility(*batch)
+                    probabilities = tf.nn.softmax(utilities, axis=-1)
+                    loss = tf.keras.losses.CategoricalCrossentropy(reduction="sum")(
+                        y_pred=probabilities,
+                        y_true=tf.one_hot(dataset.choices, depth=probabilities.shape[1]),
+                    )
+            # Compute the Jacobian
+            jacobian = tape_2.jacobian(loss, w)
+        # Compute the Hessian from the Jacobian
+        hessian = tape_1.batch_jacobian(jacobian, w)
+        return tf.sqrt(
+            [tf.linalg.inv(tf.squeeze(hessian))[i][i] for i in range(len(tf.squeeze(hessian)))]
+        )
+
+    def clone(self):
+        """Return a clone of the model."""
+        clone = NestedLogit(
+            coefficients=self.coefficients,
+            add_exit_choice=self.add_exit_choice,
+            optimizer=self.optimizer_name,
+            items_nests=self.items_nests,
+        )
+        if hasattr(self, "history"):
+            clone.history = self.history
+        if hasattr(self, "is_fitted"):
+            clone.is_fitted = self.is_fitted
+        if hasattr(self, "instantiated"):
+            clone.instantiated = self.instantiated
+        clone.loss = self.loss
+        clone.label_smoothing = self.label_smoothing
+        if hasattr(self, "report"):
+            clone.report = self.report
+        if hasattr(self, "trainable_weights"):
+            clone.trainable_weights = self.trainable_weights
+        if hasattr(self, "lr"):
+            clone.lr = self.lr
+        if hasattr(self, "_shared_features_by_choice_names"):
+            clone._shared_features_by_choice_names = self._shared_features_by_choice_names
+        if hasattr(self, "_items_features_by_choice_names"):
+            clone._items_features_by_choice_names = self._items_features_by_choice_names
+        return clone
