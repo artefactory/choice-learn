1631fbd083273d16600a1f4bedfe61273109db7e
Jules DÉSIR <desir.jules@gmail.com>
2024-09-18 15:55:55 +0200

    TST: add tests for londonpassenger_loader

--- tests/unit_tests/test_os_datasets.py
+++ tests/unit_tests/test_os_datasets.py
@@ -1,5 +1,6 @@
 """Unit testing for included Open Source datasets loaders."""
 
+import numpy as np
 import pandas as pd
 
 from choice_learn.data import ChoiceDataset
@@ -102,18 +103,220 @@ def test_londonpassenger_loader():
     """Test loading the London Passenger Mode Choice dataset."""
     londonpassenger = load_londonpassenger(as_frame=True)
     assert isinstance(londonpassenger, pd.DataFrame)
+    expected_columns = [
+        "trip_id",
+        "household_id",
+        "person_n",
+        "trip_n",
+        "travel_mode",
+        "purpose",
+        "fueltype",
+        "faretype",
+        "bus_scale",
+        "survey_year",
+        "travel_year",
+        "travel_month",
+        "travel_date",
+        "day_of_week",
+        "start_time",
+        "age",
+        "female",
+        "driving_license",
+        "car_ownership",
+        "distance",
+        "dur_walking",
+        "dur_cycling",
+        "dur_pt_access",
+        "dur_pt_rail",
+        "dur_pt_bus",
+        "dur_pt_int",
+        "pt_interchanges",
+        "dur_driving",
+        "cost_transit",
+        "cost_driving_fuel",
+        "cost_driving_ccharge",
+        "driving_traffic_percent",
+    ]
+    assert londonpassenger.columns.equals(pd.Index(expected_columns))
     assert londonpassenger.shape == (81086, 32)
 
     londonpassenger = load_londonpassenger()
     assert isinstance(londonpassenger, ChoiceDataset)
+    assert np.shape(londonpassenger.items_features_by_choice) == (1, 81086, 4, 6)
+    assert np.shape(londonpassenger.shared_features_by_choice) == (1, 81086, 15)
+    expected_items_features_names = [
+        "fueltype",
+        "faretype",
+        "bus_scale",
+        "dur",
+        "interchanges",
+        "cost_transit",
+    ]
+    assert londonpassenger.items_features_by_choice_names[0] == expected_items_features_names
+    expected_shared_features_names = [
+        "household_id",
+        "person_n",
+        "trip_n",
+        "purpose",
+        "survey_year",
+        "travel_year",
+        "travel_month",
+        "travel_date",
+        "day_of_week",
+        "start_time",
+        "age",
+        "female",
+        "driving_license",
+        "car_ownership",
+        "distance",
+    ]
+    assert londonpassenger.shared_features_by_choice_names[0] == expected_shared_features_names
+
     londonpassenger = load_londonpassenger(add_items_one_hot=True)
     assert isinstance(londonpassenger, ChoiceDataset)
+    assert np.shape(londonpassenger.items_features_by_choice) == (
+        1,
+        81086,
+        4,
+        10,
+    )  # last dimension +4
+    assert np.shape(londonpassenger.shared_features_by_choice) == (1, 81086, 15)
+    expected_items_features_names = [
+        "fueltype",
+        "faretype",
+        "bus_scale",
+        "dur",
+        "interchanges",
+        "cost_transit",
+        "oh_walking",
+        "oh_cycling",
+        "oh_pt",
+        "oh_driving",
+    ]
+    assert londonpassenger.items_features_by_choice_names[0] == expected_items_features_names
+    expected_shared_features_names = [
+        "household_id",
+        "person_n",
+        "trip_n",
+        "purpose",
+        "survey_year",
+        "travel_year",
+        "travel_month",
+        "travel_date",
+        "day_of_week",
+        "start_time",
+        "age",
+        "female",
+        "driving_license",
+        "car_ownership",
+        "distance",
+    ]
+    assert londonpassenger.shared_features_by_choice_names[0] == expected_shared_features_names
 
     londonpassenger = load_londonpassenger(as_frame=True, preprocessing="summation")
     assert isinstance(londonpassenger, pd.DataFrame)
+    expected_columns = [
+        "trip_id",
+        "household_id",
+        "person_n",
+        "trip_n",
+        "travel_mode",
+        "purpose",
+        "fueltype",
+        "faretype",
+        "bus_scale",
+        "survey_year",
+        "travel_year",
+        "travel_month",
+        "travel_date",
+        "day_of_week",
+        "start_time",
+        "age",
+        "female",
+        "driving_license",
+        "car_ownership",
+        "distance",
+        "dur_walking",
+        "dur_cycling",
+        "pt_interchanges",
+        "dur_driving",
+        "cost_pt",
+        "driving_traffic_percent",
+        "dur_pt",
+        "cost_driving",
+    ]
+    assert londonpassenger.columns.equals(pd.Index(expected_columns))
     assert londonpassenger.shape == (81086, 28)
 
     londonpassenger = load_londonpassenger(preprocessing="summation")
     assert isinstance(londonpassenger, ChoiceDataset)
+    assert np.shape(londonpassenger.items_features_by_choice) == (1, 81086, 4, 6)
+    assert np.shape(londonpassenger.shared_features_by_choice) == (1, 81086, 15)
+    expected_items_features_names = [
+        "fueltype",
+        "faretype",
+        "bus_scale",
+        "dur",
+        "interchanges",
+        "cost_transit",
+    ]
+    assert londonpassenger.items_features_by_choice_names[0] == expected_items_features_names
+    expected_shared_features_names = [
+        "household_id",
+        "person_n",
+        "trip_n",
+        "purpose",
+        "survey_year",
+        "travel_year",
+        "travel_month",
+        "travel_date",
+        "day_of_week",
+        "start_time",
+        "age",
+        "female",
+        "driving_license",
+        "car_ownership",
+        "distance",
+    ]
+    assert londonpassenger.shared_features_by_choice_names[0] == expected_shared_features_names
+
     londonpassenger = load_londonpassenger(add_items_one_hot=True, preprocessing="summation")
     assert isinstance(londonpassenger, ChoiceDataset)
+    assert np.shape(londonpassenger.items_features_by_choice) == (
+        1,
+        81086,
+        4,
+        10,
+    )  # last dimension +4
+    assert np.shape(londonpassenger.shared_features_by_choice) == (1, 81086, 15)
+    expected_items_features_names = [
+        "fueltype",
+        "faretype",
+        "bus_scale",
+        "dur",
+        "interchanges",
+        "cost_transit",
+        "oh_walking",
+        "oh_cycling",
+        "oh_pt",
+        "oh_driving",
+    ]
+    assert londonpassenger.items_features_by_choice_names[0] == expected_items_features_names
+    expected_shared_features_names = [
+        "household_id",
+        "person_n",
+        "trip_n",
+        "purpose",
+        "survey_year",
+        "travel_year",
+        "travel_month",
+        "travel_date",
+        "day_of_week",
+        "start_time",
+        "age",
+        "female",
+        "driving_license",
+        "car_ownership",
+        "distance",
+    ]
+    assert londonpassenger.shared_features_by_choice_names[0] == expected_shared_features_names
