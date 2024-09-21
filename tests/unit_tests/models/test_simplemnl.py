dcad6aa365ad24f11cb9dea9957bf50b4b7fab43
VincentAuriau <auriau.vincent@gmail.com>
2024-09-21 19:16:35 +0200

    ADD: val_dataset in tests

--- tests/unit_tests/models/test_simplemnl.py
+++ tests/unit_tests/models/test_simplemnl.py
@@ -56,7 +56,7 @@ def test_fit_adam():
     model = SimpleMNL(intercept="item", optimizer="Adam", epochs=100, lr=0.1)
     model.instantiate(n_items=3, n_items_features=2, n_shared_features=3)
     nll_b = model.evaluate(test_dataset)
-    model.fit(test_dataset, get_report=True)
+    model.fit(test_dataset, get_report=True, val_dataset=test_dataset)
     nll_a = model.evaluate(test_dataset)
     assert nll_a < nll_b
 
