a311224046fc9308fb3b78860fa697418d068368
VincentAURIAU <auriau.vincent@gmail.com>
2024-12-27 23:22:28 +0100

    FIX: pre-commits, again

--- tests/unit_tests/datasets/test_expedia.py
+++ tests/unit_tests/datasets/test_expedia.py
@@ -1,14 +1,11 @@
 """Unit testing for Expedia loader."""
 
-import numpy as np
-import pandas as pd
 import pytest
 
-from choice_learn.data import ChoiceDataset
 from choice_learn.datasets import load_expedia
 
 
 def test_raise_filenotfound():
-    """Test that error raised if no file exist"""
+    """Test that error raised if no file exist."""
     with pytest.raises(FileNotFoundError):
         load_expedia()
