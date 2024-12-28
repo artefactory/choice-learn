b19e6395d88b069eb978b799e3820ba13fb8b4f7
VincentAURIAU <auriau.vincent@gmail.com>
2024-12-28 22:32:52 +0100

    ADD: additional tests for Assortment Optimizers

--- tests/unit_tests/tools/test_base.py
+++ tests/unit_tests/tools/test_base.py
@@ -15,30 +15,32 @@ solvers = ["or-tools"]
 def test_mnl_assort_instantiate():
     """Test instantiation with both solvers."""
     for solv in solvers:
-        MNLAssortmentOptimizer(
+        opt = MNLAssortmentOptimizer(
             solver=solv,
             utilities=np.array([1.0, 2.0, 3.0]),
             itemwise_values=np.array([0.5, 0.5, 0.5]),
             assortment_size=2,
         )
+        opt.solve()
 
 
 def test_lc_assort_instantiate():
     """Test instantiation with both solvers."""
     for solv in solvers:
-        LatentClassAssortmentOptimizer(
+        opt = LatentClassAssortmentOptimizer(
             solver=solv,
             class_weights=np.array([0.2, 0.8]),
             class_utilities=np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]]),
             itemwise_values=np.array([0.5, 0.5, 0.5]),
             assortment_size=2,
         )
+        opt.solve()
 
 
 def test_lc_pricing_instantiate():
     """Test instantiation with both solvers."""
     for solv in solvers:
-        LatentClassPricingOptimizer(
+        opt = LatentClassPricingOptimizer(
             solver=solv,
             class_weights=np.array([0.2, 0.8]),
             class_utilities=np.array(
@@ -47,6 +49,7 @@ def test_lc_pricing_instantiate():
             itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
             assortment_size=2,
         )
+        opt.solve()
 
 
 def test_wrong_solver():
@@ -75,5 +78,65 @@ def test_wrong_solver():
                 [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
             ),
             itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
+            assortment_size=2,
+        )
+
+def test_raised_errors():
+    """Test diverse parametrization that should raise errors."""
+    with pytest.raises(ValueError):
+        MNLAssortmentOptimizer(
+            solver="ortools",
+            utilities=np.array([1.0, 2.0, 3.0, 4.0]),
+            itemwise_values=np.array([0.5, 0.5, 0.5]),
+            assortment_size=2,
+        )
+    with pytest.raises(ValueError):
+        LatentClassAssortmentOptimizer(
+            solver="ortools",
+            class_weights=np.array([0.2, 0.8]),
+            class_utilities=np.array([[1.0, 2.0], [3.0, 2.0]]),
+            itemwise_values=np.array([0.5, 0.5, 0.5]),
+            assortment_size=2,
+        )
+        
+    with pytest.raises(ValueError):
+        LatentClassAssortmentOptimizer(
+            solver="ortools",
+            class_weights=np.array([0.2, 0.8]),
+            class_utilities=np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0], [4.0, 4.0, 4.0]]),
+            itemwise_values=np.array([0.5, 0.5, 0.5]),
+            assortment_size=2,
+        )
+
+    with pytest.raises(ValueError):
+        LatentClassPricingOptimizer(
+            solver="ortools",
+            class_weights=np.array([0.2, 0.8]),
+            class_utilities=np.array(
+                [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
+            ),
+            itemwise_values=np.array([[0.5, 1.2, 2.4], [0.5, 1.2, 2.4], [0.5, 1.2, 2.4]]),
+            assortment_size=2,
+        )
+    
+    with pytest.raises(ValueError):
+        LatentClassPricingOptimizer(
+            solver="ortools",
+            class_weights=np.array([0.2, 0.7, 0.1]),
+            class_utilities=np.array(
+                [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
+            ),
+            itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
+            assortment_size=2,
+        )
+
+    with pytest.raises(ValueError):
+        LatentClassPricingOptimizer(
+            solver="ortools",
+            class_weights=np.array([0.2, 0.8]),
+            class_utilities=np.array(
+                [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
+            ),
+            itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
             assortment_size=2,
         )
