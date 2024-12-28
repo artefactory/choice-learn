72004a59e8f4327ea755d32570c105d8caac70a3
VincentAURIAU <auriau.vincent@gmail.com>
2024-12-28 23:18:28 +0100

    FIX&ADD: parametrizations of optimization algorithms

--- tests/unit_tests/tools/test_base.py
+++ tests/unit_tests/tools/test_base.py
@@ -23,6 +23,61 @@ def test_mnl_assort_instantiate():
         )
         opt.solve()
 
+def test_various_params():
+    """Test specific parametrizations."""
+    MNLAssortmentOptimizer(
+        solver="ortools",
+        utilities=np.array([1.0, 2.0, 3.0]),
+        itemwise_values=np.array([0.5, 0.5, 0.5]),
+        assortment_size=2,
+        outside_option_given=True,
+    )
+    LatentClassAssortmentOptimizer(
+        solver="ortools",
+        class_weights=np.array([0.2, 0.8]),
+        class_utilities=np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]]),
+        itemwise_values=np.array([0.5, 0.5, 0.5]),
+        assortment_size=12,
+        outside_option_given=True,
+    )
+    LatentClassPricingOptimizer(
+        solver="ortools",
+        class_weights=np.array([0.2, 0.8]),
+        class_utilities=np.array(
+            [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
+        ),
+        itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
+        assortment_size=12,
+        outside_option_given=True,
+    )
+
+def test_capacity_constraints():
+    """Test that capacity constraints work."""
+    opt = LatentClassAssortmentOptimizer(
+        solver="ortools",
+        class_weights=np.array([0.2, 0.8]),
+        class_utilities=np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]]),
+        itemwise_values=np.array([0.5, 0.5, 0.5]),
+        assortment_size=12,
+    )
+
+    opt.add_maximal_capacity_constraint(itemwise_capacities=[1.1, 2.2, 3.3], maximum_capacity=4.5)
+    opt.add_minimal_capacity_constraint(itemwise_capacities=[1.1, 2.2, 3.3], minimum_capacity=1.2)
+
+    opt = LatentClassPricingOptimizer(
+        solver="ortools",
+        class_weights=np.array([0.2, 0.8]),
+        class_utilities=np.array(
+            [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
+        ),
+        itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
+        assortment_size=2,
+    )
+
+    opt.add_maximal_capacity_constraint(itemwise_capacities=[1.1, 2.2, 3.3], maximum_capacity=4.5)
+    opt.add_minimal_capacity_constraint(itemwise_capacities=[1.1, 2.2, 3.3], minimum_capacity=1.2)
+
+
 
 def test_lc_assort_instantiate():
     """Test instantiation with both solvers."""
