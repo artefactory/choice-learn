eadf6320b3de5821e896cc2cfd4da6443eb9ff73
VincentAURIAU <auriau.vincent@gmail.com>
2024-12-28 21:06:49 +0100

    ADD: errors raised tests

--- tests/unit_tests/tools/test_base.py
+++ tests/unit_tests/tools/test_base.py
@@ -1,6 +1,7 @@
 """Testing base ChoiceModel."""
 
 import numpy as np
+import pytest
 
 from choice_learn.toolbox.assortment_optimizer import (
     LatentClassAssortmentOptimizer,
@@ -39,6 +40,35 @@ def test_lc_pricing_instantiate():
     for solv in solvers:
         LatentClassPricingOptimizer(
             solver=solv,
+            class_weights=np.array([0.2, 0.8]),
+            class_utilities=np.array(
+                [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
+            ),
+            itemwise_values=np.array([[0.5, 1.2], [0.5, 1.2], [0.5, 1.2]]),
+            assortment_size=2,
+        )
+
+def test_wrong_solver():
+    """Test error raised when specifying wrong solver."""
+    solver = "rotools"
+    with pytest.raises(ValueError):
+        MNLAssortmentOptimizer(
+            solver=solver,
+            utilities=np.array([1.0, 2.0, 3.0]),
+            itemwise_values=np.array([0.5, 0.5, 0.5]),
+            assortment_size=2,
+        )
+    with pytest.raises(ValueError):
+        LatentClassAssortmentOptimizer(
+            solver=solver,
+            class_weights=np.array([0.2, 0.8]),
+            class_utilities=np.array([[1.0, 2.0, 3.0], [3.0, 2.0, 1.0]]),
+            itemwise_values=np.array([0.5, 0.5, 0.5]),
+            assortment_size=2,
+        )
+    with pytest.raises(ValueError):
+        LatentClassPricingOptimizer(
+            solver=solver,
             class_weights=np.array([0.2, 0.8]),
             class_utilities=np.array(
                 [[[1.0, 1.1], [2.0, 2.1], [3.0, 3.1]], [[3.0, 3.1], [2.0, 2.1], [1.0, 1.1]]]
