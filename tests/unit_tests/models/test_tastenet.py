64d53116c9d03d2d7dabec09d7ae97c55615881b
VincentAURIAU <auriau.vincent@gmail.com>
2024-12-29 21:01:09 +0100

    ADD: basic unit tastenet tests

---
+++ tests/unit_tests/models/test_tastenet.py
@@ -0,0 +1,103 @@
+"""Simple tests for the TasteNet model."""
+
+import pytest
+
+from choice_learn.models import TasteNet
+from choice_learn.datasets import load_swissmetro
+
+customers_id, dataset = load_swissmetro(preprocessing="tastenet", as_frame=False)
+dataset = dataset[:20]
+
+taste_net_layers = []
+taste_net_activation = "relu"
+items_features_by_choice_parametrization = [[-1., "-exp", "-exp", 0., "linear", 0., 0.],
+                            [-1., "-exp", "-exp", "linear", 0., "linear", 0.],
+                            [-1., "-exp", 0., 0., 0., 0., 0.]]
+
+def test_activation():
+    """Tests TasteNet activation."""
+    tastenet = TasteNet(
+        taste_net_layers=taste_net_layers,
+        taste_net_activation=taste_net_activation,
+        items_features_by_choice_parametrization=items_features_by_choice_parametrization,
+        optimizer="Adam",
+        epochs=40,
+        lr=0.001,
+        batch_size=32,
+    )
+    for act in ["linear", "relu", "-relu", "exp", "-exp", "tanh", "sigmoid"]:
+        _ = tastenet.get_activation_function(act)
+    with pytest.raises(ValueError):
+        tastenet.get_activation_function("abc")
+
+def test_fit_adam():
+    """Test fit with Gradient Descent."""
+    tastenet = TasteNet(taste_net_layers=taste_net_layers,
+                    taste_net_activation=taste_net_activation,
+                    items_features_by_choice_parametrization=items_features_by_choice_parametrization,
+                    optimizer="Adam",
+                    epochs=5,
+                    lr=0.001,
+                    batch_size=32)
+    hist = tastenet.fit(dataset)
+    assert True
+
+def test_fit_lbfgs():
+    """Test fit with Gradient Descent."""
+    tastenet = TasteNet(taste_net_layers=taste_net_layers,
+                    taste_net_activation=taste_net_activation,
+                    items_features_by_choice_parametrization=items_features_by_choice_parametrization,
+                    optimizer="lbfgs",
+                    epochs=5,
+                    lr=0.001,
+                    batch_size=32)
+    hist = tastenet.fit(dataset)
+    assert True
+
+def test_errors_raised():
+    """Test diverse errors that should be raised."""
+    with pytest.raises(ValueError):
+        tastenet = TasteNet(taste_net_layers=taste_net_layers,
+                        taste_net_activation=taste_net_activation,
+                        items_features_by_choice_parametrization=[[-1., "-exp", "-exp", 0., "linear", 0., 0.],
+                            [-1., "-exp", "-exp", "linear", 0., "linear", 0.]],
+                        optimizer="Adam",
+                        epochs=5,
+                        lr=0.001,
+                        batch_size=32)
+        hist = tastenet.fit(dataset)
+
+    with pytest.raises(ValueError):
+        tastenet = TasteNet(taste_net_layers=taste_net_layers,
+                        taste_net_activation=taste_net_activation,
+                        items_features_by_choice_parametrization=[[-1., "-exp", "-exp", 0., "linear", 0., 0., 0.],
+                            [-1., "-exp", "-exp", "linear", 0., "linear", 0., 0.],
+                            [-1., "-exp", 0., 0., 0., 0., 0., 0.]],
+                        optimizer="Adam",
+                        epochs=5,
+                        lr=0.001,
+                        batch_size=32)
+        hist = tastenet.fit(dataset)
+
+    with pytest.raises(ValueError):
+        tastenet = TasteNet(taste_net_layers=taste_net_layers,
+                        taste_net_activation=taste_net_activation,
+                        items_features_by_choice_parametrization=[[-1., "-exp", "-exp", 0., "linear", 0., 0.],
+                            [-1., "-exp", "-exp", "linear", 0., "linear", 0.]],
+                        optimizer="lbfgs",
+                        epochs=5,
+                        lr=0.001,
+                        batch_size=32)
+        hist = tastenet.fit(dataset)
+
+    with pytest.raises(ValueError):
+        tastenet = TasteNet(taste_net_layers=taste_net_layers,
+                        taste_net_activation=taste_net_activation,
+                        items_features_by_choice_parametrization=[[-1., "-exp", "-exp", 0., "linear", 0., 0., 0.],
+                            [-1., "-exp", "-exp", "linear", 0., "linear", 0., 0.],
+                            [-1., "-exp", 0., 0., 0., 0., 0., 0.]],
+                        optimizer="lbfgs",
+                        epochs=5,
+                        lr=0.001,
+                        batch_size=32)
+        hist = tastenet.fit(dataset)
