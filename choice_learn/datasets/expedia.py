9586cb9de89c02c87c361f44aa29037660e2d7f5
VincentAURIAU <auriau.vincent@gmail.com>
2025-01-03 21:54:13 +0100

    fix

--- choice_learn/datasets/expedia.py
+++ choice_learn/datasets/expedia.py
@@ -10,8 +10,8 @@ from choice_learn.data.choice_dataset import ChoiceDataset
 from choice_learn.data.storage import OneHotStorage
 from choice_learn.datasets.base import get_path
 
-DATA_MODULE = "choice_learn.datasets.data"
-CACHE_MODULE = "choice_learn.datasets.cache"
+DATA_MODULE = "choice_learn/datasets/data"
+CACHE_MODULE = "choice_learn/datasets/cache"
 
 
 def load_expedia(as_frame=False, preprocessing="rumnet"):
