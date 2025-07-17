07ced795424dd923ec7f2fe84db94cfe9ca8ab7d
SouleimanS <souleiman.sbai@polytechnique.edu>
2025-07-17 14:48:27 +0200

    main refresh

--- python/main.py
+++ python/main.py
@@ -3,8 +3,8 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 
-from data import SyntheticDataGenerator
-from model import BaseModel
+from DataGen import SyntheticDataGenerator
+from AttnModel import BaseModel
 
 
 data_gen = SyntheticDataGenerator()
