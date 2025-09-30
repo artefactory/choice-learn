349dc48193eccb4c9428b06e601cfdcc09b58ce6
michaelteboul <230188763+michaelteboul@users.noreply.github.com>
2025-09-30 16:56:06 +0200

    ADD: Bakery dataset (#266)

--- tests/unit_tests/test_os_datasets.py
+++ tests/unit_tests/test_os_datasets.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 
+from choice_learn.basket_models.data import TripDataset
 from choice_learn.data import ChoiceDataset
 from choice_learn.datasets import (
     load_car_preferences,
@@ -15,6 +16,7 @@ from choice_learn.datasets import (
     load_tafeng,
     load_train,
 )
+from choice_learn.datasets.bakery import load_bakery
 from choice_learn.datasets.base import load_csv, load_gzip, slice_from_names
 
 
@@ -432,3 +434,11 @@ def test_load_csv():
     _ = load_csv(data_file_name="test_data.csv", data_module="tests/data")
     names, data = load_gzip("swissmetro.csv.gz", data_module="choice_learn/datasets/data")
     _ = slice_from_names(data, names[:4], names)
+
+
+def test_bakery_loader():
+    """Test loading the Bakery dataset."""
+    bakery = load_bakery(as_frame=True)
+    assert isinstance(bakery, pd.DataFrame)
+    bakery = load_bakery()
+    assert isinstance(bakery, TripDataset)
