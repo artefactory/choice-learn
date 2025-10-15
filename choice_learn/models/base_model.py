1ecfaeff6171856f53935084cce597c27ca43070
Vincent Auriau <auriau.vincent@gmail.com>
2025-10-16 00:16:46 +0200

    ADD: optimizer state can now be saved & reinstantiated (#279)

--- choice_learn/models/base_model.py
+++ choice_learn/models/base_model.py
@@ -516,7 +516,7 @@ class ChoiceModel:
         }
         return batch_loss, probabilities
 
-    def save_model(self, path):
+    def save_model(self, path, save_opt=True):
         """Save the different models on disk.
 
         Parameters
@@ -538,16 +538,34 @@ class ChoiceModel:
             elif isinstance(v, (list, tuple)):
                 if all(isinstance(item, (int, float, str, dict)) for item in v):
                     params[k] = v
-                else:
+                elif k != "_trainable_weights":
                     logging.warning(
                         """Attribute '%s' is a list with non-serializable
                          types and will not be saved.""",
                         k,
                     )
-        with open(os.path.join(path, "params.json"), "w") as f:
+        with open(Path(path) / "params.json", "w") as f:
             json.dump(params, f)
 
         # Save optimizer state
+        if save_opt and not isinstance(self.optimizer, str):
+            (Path(path) / "optimizer").mkdir(parents=True, exist_ok=True)
+            config = self.optimizer.get_config()
+            weights_store = {}
+            self.optimizer.save_own_variables(weights_store)
+            for key, value in weights_store.items():
+                if isinstance(value, tf.Variable):
+                    value = value.numpy()
+                weights_store[key] = value.tolist()
+            if "learning_rate" in config.keys():
+                if isinstance(config["learning_rate"], tf.Variable):
+                    config["learning_rate"] = config["learning_rate"].numpy()
+                if isinstance(config["learning_rate"], np.float32):
+                    config["learning_rate"] = config["learning_rate"].tolist()
+            with open(Path(path) / "optimizer" / "config.json", "w") as f:
+                json.dump(config, f)
+            with open(Path(path) / "optimizer" / "weights_store.json", "w") as f:
+                json.dump(weights_store, f)
 
     @classmethod
     def load_model(cls, path):
@@ -563,7 +581,11 @@ class ChoiceModel:
         ChoiceModel
             Loaded ChoiceModel
         """
-        obj = cls()
+        # To improve for non string attributes
+        with open(Path(path) / "params.json") as f:
+            params = json.load(f)
+
+        obj = cls(optimizer=params["optimizer_name"])
         obj._trainable_weights = []
 
         i = 0
@@ -576,10 +598,21 @@ class ChoiceModel:
             i += 1
             weight_path = f"weight_{i}.npy"
 
-        # To improve for non string attributes
-        params = json.load(open(Path(path) / "params.json"))
         for k, v in params.items():
             setattr(obj, k, v)
+
+        if Path.is_dir(Path(path) / "optimizer"):
+            with open(Path(path) / "optimizer" / "config.json") as f:
+                config = json.load(f)
+            # obj.optimizer = tf.keras.optimizers.get(params["optimizer_name"]).from_config(config)
+            obj.optimizer = obj.optimizer.from_config(config)
+            obj.optimizer.build(var_list=obj.trainable_weights)
+
+            with open(Path(path) / "optimizer" / "weights_store.json") as f:
+                store = json.load(f)
+            for key, value in store.items():
+                store[key] = np.array(value, dtype=np.float32)
+            obj.optimizer.load_own_variables(store)
 
         # Load optimizer step
         return obj
